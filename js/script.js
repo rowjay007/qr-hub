b76e133e35985fe98f0a753fde4e68c8f8e51d56
rowjay007 <rowlives@gmail.com>
2022-08-30 12:08:49 +0100

    up and running code

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// @ts-nocheck
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
+
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
+
+  clearUI();
+
+  const url = document.getElementById("url").value;
+  const size = document.getElementById("size").value;
+
+  if (url === "") {
+    alert("Please enter a URL");
+  } else {
+    showSpinner();
+    setTimeout(() => {
+      hideSpinner();
+      generateQRCode(url, size);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector("img").src;
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode("qrcode", {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+const clearUI = () => {
+  qr.innerHTML = "";
+  const saveBtn = document.getElementById("save-link");
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
+
+const showSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+};
+
+const hideSpinner = () => {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
+};
+
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement("a");
+  link.id = "save-link";
+  link.classList =
+    "bg-[#006333] hover:bg-green-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+  link.href = saveUrl;
+  link.download = "qrcode";
+  link.innerHTML = "Save Image";
+  document.getElementById("generated").appendChild(link);
+};
+
+hideSpinner();
+
+form.addEventListener("submit", onGenerateSubmit);
